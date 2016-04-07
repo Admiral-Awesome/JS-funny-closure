b9ded2731cf4d5e5f72f14c6f1077bb58a8bfe63
Admiral-Awesome <orlovan17@gmail.com>
2016-04-07 23:40:25 +0300

    closure
    closure

---
+++ js/closure.js
@@ -0,0 +1,29 @@
+    function sum(val) {
+        var value = val
+
+        return function sum(val2){
+            if (val2 === undefined){
+                
+                return value;
+                }
+
+                value += val2;
+                return sum;
+
+        }
+    }
+    function sum2(val) {
+        var value = val;
+
+        return {
+            getVal : function() {
+                return value;
+            }, add : function(val2) {
+                if ( val2 === undefined ) {
+                    return getVal();
+                }
+                value += val2;
+                return value;
+            }
+        }
+}
